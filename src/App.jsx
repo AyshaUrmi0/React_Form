5f9ec39484a46ade1a74e03ea997d430944de173
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-11-01 22:18:06 +0900

    form created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>React Form</h1>
+     
+      <SimpleForm />
+    </>
+  )
+}
+
+export default App
